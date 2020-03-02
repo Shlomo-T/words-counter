63700c3f5ffb9fe9697f4a35b56c59944f5aa631
shlomotadela <shlontz@gmail.com>
2020-03-02 21:52:48 +0200

    - Changed celery declaration to use settings configuration - Changed terminology in word-counter api (method->worker) - Refactored word-statistics api to use GET method with url params - Refactor celery task to not catch exception (throw it in worker logs instead and show traceback)

--- app.py
+++ app.py
@@ -2,51 +2,48 @@ from flask import Flask, request
 from workers import *
 from data_manager import DataManager
 from celery import Celery
+from settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 
 app = Flask(__name__)
 
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/1'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/1'
+app.config['CELERY_BROKER_URL'] = CELERY_BROKER_URL
+app.config['CELERY_RESULT_BACKEND'] = CELERY_RESULT_BACKEND
 app.config['CELERY_TRACK_STARTED'] = True
 app.config['CELERY_SEND_EVENTS'] = True
 
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery = Celery(app.name, broker=CELERY_BROKER_URL)
 celery.conf.update(app.config)
 
 
-allowed_methods = {'text': RawTextWorker, 'url': WebUrlWorker, 'file': FileSystemWorker}
+allowed_workers = {'text': RawTextWorker, 'url': WebUrlWorker, 'file': FileSystemWorker}
 
 
 @celery.task
 def naive_worker_execute(input_method, input_data):
-    worker = allowed_methods[input_method](input_data)
-    try:
-        worker.invoke()
-    except Exception as ex:
-        return repr(ex), 400
+    worker = allowed_workers[input_method](input_data)
+    worker.invoke()
 
 
 @app.route('/word-counter', methods=['POST'], strict_slashes=False)
 def word_counter():
     payload = request.json
-    if not payload or 'method' not in payload or payload['method'] not in allowed_methods or 'parameter' not in payload:
+    if not payload or 'worker' not in payload or payload['worker'] not in allowed_workers or 'parameter' not in payload:
         return 'Bad Input', 400
-    input_method = payload['method']
+    worker = payload['worker']
     input_data = payload['parameter']
-    naive_worker_execute.delay(input_method,input_data)
+    naive_worker_execute.delay(worker, input_data)
     return 'OK!', 200
 
 
-@app.route('/word-statistics', methods=['POST'], strict_slashes=False)
+@app.route('/word-statistics', methods=['GET'], strict_slashes=False)
 def word_statistics():
-    payload = request.json
-    if not payload or 'keyword' not in payload:
-        return 'Bad Input', 400
-    key = payload['keyword'].lower()
-    value = DataManager().get_key(key=key)
-    response_body = {'keyword': key, 'amount': value if value else 0}
+    keyword = request.args.get('keyword')
+    if not keyword:
+        return 'Bad Input: Keyword parameter is missing', 400
+    value = DataManager().get_key(key=keyword.lower())
+    response_body = {'keyword': keyword, 'amount': value if value else 0}
     return response_body, 200
 
 
