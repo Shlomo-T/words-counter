568dc321486787ff4cd43156195c661fb3d60b73
shlomotadela <shlontz@gmail.com>
2020-03-02 21:44:44 +0200

    DataManager class to use redis with settings configuration

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,11 @@
 from redis.client import StrictRedis
+from settings import REDIS_PERSISTENT_DB
 
 
 class DataManager:
 
-    client = StrictRedis(decode_responses=True)
+    client = StrictRedis.from_url(REDIS_PERSISTENT_DB, decode_responses=True)
     words_counter = 'words'
-    jobs_hash = 'jobs'
 
     def increment_key(self, key, amount):
         self.client.hincrby(name=self.words_counter, key=key, amount=amount)
